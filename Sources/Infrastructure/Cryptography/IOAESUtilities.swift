7ab7299abceeec857ab84c19b9e8a3829cf8e958
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2022-10-01 20:34:26 +0300

    RSA and AES utilities was added.

---
+++ Sources/Infrastructure/Cryptography/IOAESUtilities.swift
@@ -0,0 +1,97 @@
+//
+//  IOAESUtilities.swift
+//  
+//
+//  Created by Adnan ilker Ozcan on 30.09.2022.
+//
+
+import CommonCrypto
+import Foundation
+
+final public class IOAESUtilities {
+ 
+    // MARK: - AES
+    
+    public static func encrypt(data: Data, keyData: Data, ivData: Data?) -> Data? {
+        return self.crypt(data: data, algorithm: CCAlgorithm(kCCAlgorithmAES), operation: CCOperation(kCCEncrypt), key: keyData, iv: ivData)
+    }
+    
+    public static func encrypt(string: String, keyData: Data, ivData: Data?) -> Data? {
+        if let data = string.data(using: .utf8) {
+            return self.encrypt(data: data, keyData: keyData, ivData: ivData)
+        }
+        
+        return nil
+    }
+    
+    public static func decrypt(data: Data, keyData: Data, ivData: Data?) -> Data? {
+        return self.crypt(data: data, algorithm: CCAlgorithm(kCCAlgorithmAES), operation: CCOperation(kCCDecrypt), key: keyData, iv: ivData)
+    }
+    
+    public static func decrypt(string: String, keyData: Data, ivData: Data?) -> Data? {
+        if let data = string.data(using: .utf8) {
+            return self.decrypt(data: data, keyData: keyData, ivData: ivData)
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Helper Methods
+    
+    private static func crypt(data: Data, algorithm: CCAlgorithm, operation: CCOperation, key: Data, iv: Data?) -> Data? {
+        // Convert key and iv to data
+        let dataIn = NSMutableData(data: data)
+        let keyData = NSMutableData(data: key)
+        let ivData: NSMutableData
+        
+        // Create algorithm size
+        var dataMoved = 0
+        let algorithmSize = self.size(forAlgorithm: algorithm)
+        
+        // Create decrypted data
+        let cryptedDataCapacity = data.count + algorithmSize
+        let cryptedData = NSMutableData(length: cryptedDataCapacity)!
+        
+        // Create options
+        var options = CCOptions(kCCOptionPKCS7Padding | kCCOptionECBMode)
+        
+        // Check iv is not null
+        if let iv {
+            // Then update options
+            options = CCOptions(kCCOptionPKCS7Padding)
+            ivData = NSMutableData(data: iv)
+        } else {
+            ivData = NSMutableData()
+        }
+        
+        // Crypt data
+        let status = CCCrypt(operation, algorithm, options, keyData.bytes, keyData.length, ivData.bytes, dataIn.bytes, dataIn.length, cryptedData.mutableBytes, cryptedDataCapacity, &dataMoved)
+
+        // Check result
+        if status == kCCSuccess {
+            // Then update data size
+            cryptedData.length = dataMoved
+            return cryptedData as Data
+        }
+        
+        return nil
+    }
+    
+    private static func size(forAlgorithm algorithm: CCAlgorithm) -> Int {
+        // Create size value
+        var size = 0
+        
+        // Check algorithms
+        if algorithm == kCCAlgorithmAES128 || algorithm == kCCAlgorithmAES {
+            size = kCCBlockSizeAES128
+        } else if algorithm == kCCAlgorithmDES {
+            size = kCCBlockSizeDES
+        } else if algorithm == kCCAlgorithm3DES {
+            size = kCCBlockSize3DES
+        } else if algorithm == kCCAlgorithmCAST {
+            size = kCCBlockSizeCAST
+        }
+        
+        return size
+    }
+}
--- Sources/Infrastructure/Cryptography/IORSAEncryptionUtilities.swift
+++ Sources/Infrastructure/Cryptography/IORSAEncryptionUtilities.swift
@@ -16,66 +16,24 @@ final public class IORSAEncryptionUtilities {
     }
     
     public static func encrypt(data: Data, publicKey: SecKey, rsaPaddingType: PaddingType = .pkcs1) -> Data? {
-        // Plain text bytes
-        let plainText = data.bytes
-        let plainTextLen = data.count
+        // Convert data
+        var plainData = data as CFData
         
-        // Create cipher text and block size
-        let blockSize = SecKeyGetBlockSize(publicKey) * MemoryLayout<UInt8>.size
-        var cipherText: [UInt8] = Array(repeating: 0, count: blockSize)
-        
-        var blockSizePerEncrypt = 0
-        var paddingType = SecPadding(rawValue: 0)
+        // Create an algorithm
+        let algorithm: SecKeyAlgorithm
         
         // Check use pkcs1 padding
         if rsaPaddingType == .pkcs1 {
-            // When PKCS1 padding is performed, the maximum length of data that can
-            // be encrypted is the value returned by SecKeyGetBlockSize() - 11.
-            blockSizePerEncrypt = blockSize - 11
-            paddingType = .PKCS1
-        } else if rsaPaddingType == .oaep {
-            // When OAEP padding is performed, the maximum length of data that can
-            // be encrypted is the value returned by SecKeyGetBlockSize() - 2 - (2 * hash size).
-            blockSizePerEncrypt = blockSize - 2 - (2 * 20)
-            paddingType = .OAEP
+            algorithm = .rsaEncryptionPKCS1
+        } else {
+            algorithm = .rsaEncryptionOAEPSHA1
         }
         
-        // Create encrypted data
-        var accumulatedEncryptedData = Data()
+        // swiftlint:disable redundant_optional_initialization
+        var error: Unmanaged<CFError>? = nil
+        let encryptedData = SecKeyCreateEncryptedData(publicKey, algorithm, plainData, &error)
+        // swiftlint:enable redundant_optional_initialization
         
-        // Create block sizes
-        var needEncryptLen = 0
-        var realEncryptLen = 0
-        
-        // Loop throught ciphers
-        for i in stride(from: 0, to: plainTextLen, by: blockSizePerEncrypt) {
-            // Calculate block sizes
-            needEncryptLen = plainTextLen - i
-            realEncryptLen = blockSize
-            
-            // Check needed encrypt length is greater than block size
-            if needEncryptLen > blockSizePerEncrypt {
-                needEncryptLen = blockSizePerEncrypt
-            }
-            
-            // Encrypt text
-            var plainTextStripped = plainText.dropFirst(i)
-            let status = SecKeyEncrypt(publicKey, paddingType, &plainTextStripped, needEncryptLen, &cipherText[i], &realEncryptLen)
-            
-            // Check status is success
-            if status == errSecSuccess {
-                // Append encrypted bytes
-                accumulatedEncryptedData.append(&cipherText, count: realEncryptLen)
-            } else {
-                // Break the loop
-                break
-            }
-            
-            // Update values
-            realEncryptLen = 0
-            cipherText = Array(repeating: 0, count: blockSize)
-        }
-        
-        return accumulatedEncryptedData
+        return encryptedData as Data?
     }
 }
