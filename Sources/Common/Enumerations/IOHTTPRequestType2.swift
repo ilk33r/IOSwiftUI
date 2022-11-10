cce32a434e451c23432be67d10dfd8814bcd98ad
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2022-11-10 21:56:35 +0300

    Http request type class name was temporary changed.

---
+++ Sources/Common/Enumerations/IOHTTPRequestType2.swift
@@ -0,0 +1,17 @@
+//
+//  IOHTTPRequestType.swift
+//  
+//
+//  Created by Adnan ilker Ozcan on 24.09.2022.
+//
+
+import Foundation
+
+public enum IOHTTPRequestType: String {
+    
+    case `get` = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
