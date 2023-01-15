ac79cc8c5efa3cb271063c52514be99e2e3fcc3a
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2023-01-15 22:06:07 +0300

    Validation custom message rule was added.

--- Sources/Presentation/Validation/Rules/IOValidationCustomMessageRule.swift
+++ Sources/Presentation/Validation/Rules/IOValidationCustomMessageRule.swift
@@ -1,8 +1,45 @@
 //
-//  File.swift
+//  IOValidationCustomMessageRule.swift
 //  
 //
 //  Created by Adnan ilker Ozcan on 15.01.2023.
 //
 
 import Foundation
+import IOSwiftUIInfrastructure
+import SwiftUI
+
+public struct IOValidationCustomMessageRule: IOValidationRule {
+
+    public var errorMessage: String {
+        get {
+            return errorMessageBinder
+        }
+        set {
+            _errorMessage = newValue
+        }
+    }
+
+    // MARK: - Privates
+    
+    public var _errorMessage: String
+    @Binding private var errorMessageBinder: String
+    
+    // MARK: - Initialization Methods
+    
+    public init(errorMessage: IOLocalizationType) {
+        self._errorMessage = errorMessage.localized
+        self._errorMessageBinder = Binding.constant("")
+    }
+    
+    public init(errorMessageBinder: Binding<String>) {
+        self._errorMessage = ""
+        self._errorMessageBinder = errorMessageBinder
+    }
+    
+    // MARK: - Validation Methods
+    
+    public func validate(value: String?) -> Bool {
+        return errorMessage.isEmpty
+    }
+}
