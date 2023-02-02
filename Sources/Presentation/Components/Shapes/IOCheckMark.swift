8eda0e295b3655d471525db689cb51732e00fb76
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2023-02-02 13:58:02 +0300

    CheckMark shape was added.

---
+++ Sources/Presentation/Components/Shapes/IOCheckMark.swift
@@ -0,0 +1,34 @@
+//
+//  IOCheckMark.swift
+//  
+//
+//  Created by Adnan ilker Ozcan on 2.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+public struct IOCheckMark: Shape {
+    
+    // MARK: - Initialization Methods
+    
+    public init() {
+        
+    }
+    
+    // MARK: - Drawing
+    
+    public func path(in rect: CGRect) -> Path {
+        var path = Path()
+                
+        let width = rect.size.width
+        let height = rect.size.height
+        
+        // Create check mark lines
+        path.move(to: CGPoint(x: width/3.1578, y: height/2))
+        path.addLine(to: CGPoint(x: width/2.0618, y: height/1.57894))
+        path.addLine(to: CGPoint(x: width/1.3953, y: height/2.7272))
+        
+        return path
+    }
+}
